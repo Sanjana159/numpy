29675b78574a9b456377ae08640e528ea1884fbd
Sanjana159 <158991352+Sanjana159@users.noreply.github.com>
2024-12-08 01:16:14 +0530

    Add files via upload

---
+++ one dimensional.py
@@ -0,0 +1,14 @@
+import numpy as np
+array = np.array([10,20,30,40,50])
+add = array + 3
+square = array** 2
+
+max_value = np.max(array)
+min_value = np.min(array)
+
+sum = np.sum(array)
+print("After Adding 3:", add)
+print("Squared Elements:", square)
+print("Maximum Value:", max_value)
+print("Minimum Value:", min_value)
+print("Sum of Elements:", sum)
